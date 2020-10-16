b1ecd900b5047b3d0c6b586d9d7f47a0a8b9834a
jason-nordheim <jason.nordheim@gmail.com>
2020-10-16 09:25:07 -0600

    added edge case of an ending with an empty to string, which is expected to return true

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -8,25 +8,31 @@
    // if the string provided is short then the ending 
    // string, return false 
    if (str.length < ending.length) {
-     return false 
-   } else if (str[str.length -1] !== ending[ending.length - 1]) {
-     // if the last character in the ending does not match the 
-     // the last character of the ending, return false 
-     return false 
+     return false;
+   } else if (ending.length === 0) {
+     // if the ending provided a string of length 0
+     // it would be the same ending
+     return true;
+   } else if (str[str.length - 1] !== ending[ending.length - 1]) {
+     // if the last character in the ending does not match the
+     // the last character of the ending, return false
+     return false;
    } else {
-     // compare each letter starting from the end of the 
-     // the "ending" string, if any of the characters 
-     // within the do not match 
-     // return true 
-     let distanceFromEnd = 0 
-     while (distanceFromEnd < ending.length){
-      if(str[str.length - 1 - distanceFromEnd] !== ending[ending.length - 1 - distanceFromEnd]){
-        return false 
-      }
-      distanceFromEnd++ 
+     // compare each letter starting from the end of the
+     // the "ending" string, if any of the characters
+     // within the do not match
+     // return true
+     let distanceFromEnd = 0;
+     while (distanceFromEnd < ending.length) {
+       if (
+         str[str.length - 1 - distanceFromEnd] !==
+         ending[ending.length - 1 - distanceFromEnd]
+       ) {
+         return false;
+       }
+       distanceFromEnd++;
      }
-     
-     // if we made it here, we found no different letters 
-     return true 
+     // if we made it here, we found no different letters
+     return true;
    }
  }
